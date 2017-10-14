c76248ddd37e0bc8fb54b00d0c1e856918a97463
Mike Orr <mikeorr85@gmail.com>
2017-10-14 10:37:45 -0600

    Clean up/delete cloudinary asset when deleted in app

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -5,4 +5,14 @@ class Upload < ApplicationRecord
   validates :name, presence: true
 
   belongs_to :user
+
+  before_destroy :delete_cloudinary_asset
+
+  def delete_cloudinary_asset
+    if self.image?
+      Cloudinary::Uploader.destroy(image.my_public_id)
+    elsif self.video?
+      Cloudinary::Uploader.destroy(video.my_public_id)
+    end
+  end
 end
