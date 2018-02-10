2915e1daf32bf3d4131b1444c3abb942906117f4
Joe Schafer <joeschaferastula@gmail.com>
2018-02-10 14:32:24 -0600

    removed the notifications

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(version: 20171114134655) do
 
   create_table "hearts", force: :cascade do |t|
     t.bigint "user_id"
-    t.datetime "created_at", default: "2018-02-09 21:47:50", null: false
-    t.datetime "updated_at", default: "2018-02-09 21:47:50", null: false
+    t.datetime "created_at", default: "2018-02-10 20:25:02", null: false
+    t.datetime "updated_at", default: "2018-02-10 20:25:02", null: false
     t.bigint "project_id"
     t.index ["project_id"], name: "index_hearts_on_project_id"
     t.index ["user_id"], name: "index_hearts_on_user_id"
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 20171114134655) do
   create_table "uploads", force: :cascade do |t|
     t.string "image"
     t.string "video"
-    t.datetime "created_at", default: "2018-02-09 21:47:50", null: false
-    t.datetime "updated_at", default: "2018-02-09 21:47:50", null: false
+    t.datetime "created_at", default: "2018-02-10 20:25:02", null: false
+    t.datetime "updated_at", default: "2018-02-10 20:25:02", null: false
     t.bigint "project_id"
     t.index ["project_id"], name: "index_uploads_on_project_id"
   end
