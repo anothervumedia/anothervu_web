c11006ccbb3d65d7083ede25bb84df7c9b5a98fc
Joe Schafer <joeschaferastula@gmail.com>
2018-02-28 19:57:29 -0700

    converted coffee script into javascript

--- app/assets/javascripts/pages/home.js
+++ app/assets/javascripts/pages/home.js
@@ -1,8 +1,10 @@
-$("form#login-box").bind "ajax:success", (e, data, status, xhr) ->
-    if data.success
+$("form#login-box").bind ("ajax:success", function (e, data, status, xhr) {
+    if ( data.success ) {
       //javascript that executes if everything goes o.k.
-      $('#sign_in').modal('hide')
-      $('#sign_in_button').hide()
-      $('#submit_comment').slideToggle(1000, "easeOutBack" )
-    else
-      alert('failure!')
+      $('#sign_in').modal('hide');
+      $('#sign_in_button').hide();
+      $('#submit_comment').slideToggle(1000, "easeOutBack" );
+    } else {
+      alert('failure!');
+    }
+}
