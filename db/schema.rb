1f91404e419dfb55470ed44b2e5e4683092dc764
Joe Schafer <joeschaferastula@gmail.com>
2018-03-21 19:03:32 -0500

    removed video uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320003214) do
+ActiveRecord::Schema.define(version: 20180317040955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "hearts", force: :cascade do |t|
     t.bigint "user_id"
-    t.datetime "created_at", default: "2018-02-09 20:49:24", null: false
-    t.datetime "updated_at", default: "2018-02-09 20:49:24", null: false
+    t.datetime "created_at", default: "2018-02-10 20:25:02", null: false
+    t.datetime "updated_at", default: "2018-02-10 20:25:02", null: false
     t.bigint "project_id"
     t.index ["project_id"], name: "index_hearts_on_project_id"
     t.index ["user_id"], name: "index_hearts_on_user_id"
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 20180320003214) do
   create_table "uploads", force: :cascade do |t|
     t.string "image"
     t.string "video"
-    t.datetime "created_at", default: "2018-02-09 20:49:24", null: false
-    t.datetime "updated_at", default: "2018-02-09 20:49:24", null: false
+    t.datetime "created_at", default: "2018-02-10 20:25:02", null: false
+    t.datetime "updated_at", default: "2018-02-10 20:25:02", null: false
     t.bigint "project_id"
     t.index ["project_id"], name: "index_uploads_on_project_id"
   end
