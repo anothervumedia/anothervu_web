7c871e1db927bf3f266709c855116b3f00a74372
Joe Schafer <joeschaferastula@gmail.com>
2018-03-25 14:42:48 -0500

    added website to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325191943) do
+ActiveRecord::Schema.define(version: 20180325194201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20180325191943) do
     t.string "last_name"
     t.text "biography"
     t.string "phone_number"
+    t.string "website"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
