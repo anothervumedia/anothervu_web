3c17784fe799f4beef9cc653f33224cb07072e58
Joe Schafer <joeschaferastula@gmail.com>
2018-03-25 17:31:47 -0500

    added html and html links

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  before_validation :smart_add_url_protocol
+
   has_many :projects, dependent: :destroy
   has_many :hearts, dependent: :destroy
 
 
   protected
+
+    def smart_add_url_protocol
+      unless self.website[/\Ahttp:\/\//] || self.website[/\Ahttps:\/\//]
+        self.website = "http://#{self.website}"
+      end
+    end
 
     def confirmation_required?
      false
